6c7499c0f40b1640bbdb019e98b5d1d69efdde43
Omar <83602375+obejjada@users.noreply.github.com>
2023-05-12 20:02:33 +0100

    create csv_parser.py file
    create a basic csv_parser.py file with class and a corresponding unittest file

---
+++ source/csv_parser.py
@@ -0,0 +1,7 @@
+import csv
+import logging
+
+
+class CSVParser():
+    """Class that parses the inputed CSV file and calculates the necessary data"""
+    pass
---
+++ test/test_csv_parser.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestCSVParser(unittest.TestCase):
+    """Unit test class that will verfiy the CSV parsing functions of the sky-take-home projects"""
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
