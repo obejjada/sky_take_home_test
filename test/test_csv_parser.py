b91242334857b69a797ffd07736e6c2fc85ca9a3
Omar <83602375+obejjada@users.noreply.github.com>
2023-05-12 23:25:16 +0100

    create csv_valid test
    create csv_valid test ready for code implementation.  This test will take a valid.csv file and assert that the file is a CSV file

--- test/test_csv_parser.py
+++ test/test_csv_parser.py
@@ -1,8 +1,16 @@
 import unittest
+import sys
+sys.path.append("C:\\Users\\omarb\\vscode-workspace\\sky_take_home_test")
+from source.csv_parser import CSVParser
+from pathlib import Path
+
+
+resouce_path = Path.cwd()
 
 
 class TestCSVParser(unittest.TestCase):
     """Unit test class that will verfiy the CSV parsing functions of the sky-take-home projects"""
+    csv_parser = CSVParser()
 
     def setUp(self):
         """Method thar runs before each test method is run"""
@@ -14,7 +22,9 @@ class TestCSVParser(unittest.TestCase):
 
     def test_valid_csv(self):
         """Test method to validate the valid_csv method executes as expected"""
-        pass
+        valid_csv_path = str(resouce_path) + r'\valid.csv'
+        fail_msg = '%s does not exist', valid_csv_path
+        self.assertTrue(self.csv_parser.valid_csv(valid_csv_path), fail_msg)
 
 
 if __name__ == "__main__":
