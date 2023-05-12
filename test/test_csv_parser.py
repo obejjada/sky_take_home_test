036cbd3e2259e2b2b9d0a379cf27d5b1df8ea425
Omar <83602375+obejjada@users.noreply.github.com>
2023-05-12 23:40:44 +0100

    create invalid csv test
    create an invalid csv test to check the the valid_csv method returns False when a file that does not contain .csv suffix is passed through the function

--- test/test_csv_parser.py
+++ test/test_csv_parser.py
@@ -26,6 +26,12 @@ class TestCSVParser(unittest.TestCase):
         fail_msg = '%s is not a csv file', valid_csv_path
         self.assertTrue(self.csv_parser.valid_csv(valid_csv_path), fail_msg)
 
+    def test_invalid_csv(self):
+        """Test method to validate the valid_csv method returns False when a none CSV file path is given"""
+        invalid_csv_path = str(resouce_path) + r'\invalid.txt'
+        fail_msg = '%s is not a csv file', invalid_csv_path
+        self.assertFalse(self.csv_parser.valid_csv(invalid_csv_path), fail_msg)
+
 
 if __name__ == "__main__":
     unittest.main()
