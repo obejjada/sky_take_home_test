d298333049d6a7d11e9ce43ac18f8f310e1b2ee2
Omar <83602375+obejjada@users.noreply.github.com>
2023-05-12 23:42:20 +0100

    implement valid_csv method
    implement the valid_csv method. Inputs to the method are the file path the method returns either true if the file has suffix .csv else False.

--- source/csv_parser.py
+++ source/csv_parser.py
@@ -1,9 +1,14 @@
 import csv
+from pathlib import Path
 import logging
 
 
 class CSVParser():
     """Class that parses the inputed CSV file and calculates the necessary data"""
 
-    def valid_csv(self):
-        pass
+    def valid_csv(self, file_path):
+        """Method to validate whether the input file is a CSV file or not"""
+        if Path(file_path).suffix == '.csv':
+            return True
+        else:
+            return False
