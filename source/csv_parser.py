8cb8174e99407e2d435b7a96e5a16f26a37b23d7
Omar <83602375+obejjada@users.noreply.github.com>
2023-05-13 18:37:26 +0100

    implement the common_time_hotest method
    implement the common_time_hotest which queries the database to return the time the hottest temperature was recorded on each day. The logic loops through each of the days and counts the most common occurrences before returning that

--- source/csv_parser.py
+++ source/csv_parser.py
@@ -79,3 +79,25 @@ class CSVParser():
         # Average time is calculated by summing how many hours from 00:00 have elapased before dividing by the number of days and re-convert back into a 24Hr format
         average_time = str(datetime.timedelta(seconds=total_time/len(rows)))
         return average_time
+
+    def common_time_hotest(self, database_path):
+        """Method to calculate the most common time the hottest time of the day occurs"""
+        connection = sqlite3.connect('weather_data.db')
+        cursor = connection.cursor()
+        # SQL query returns the time the maximum temperature occured for the month
+        cursor.execute("SELECT Time, MAX(Outside_Temperature) FROM weather_data GROUP BY Date")
+        rows = cursor.fetchall()
+        connection.close()
+
+        remove_duplicates_time = set()
+        time_list = []
+        max_count = 0
+        for row in rows:
+            remove_duplicates_time.add(row[0])  # Creating set will remove duplicates which can be looped through rows to count the time with the most matches
+            time_list.append(row[0])  # This list will allow looping through the times and not the temperatures
+        for i in remove_duplicates_time:
+            if time_list.count(i) > max_count:  # If the number of occurances of i is greater than the last max count, then update the max
+                max_time = i
+                max_count = time_list.count(i)
+
+        return max_time
