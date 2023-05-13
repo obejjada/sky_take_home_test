ce069abdcb15c71b06236d8e34ba85533d760993
Omar <83602375+obejjada@users.noreply.github.com>
2023-05-13 20:55:58 +0100

    create interface module
    Create interface module used to define methods that will interact with the user with prompts and errors messages

---
+++ source/interface.py
@@ -0,0 +1,6 @@
+class user_interface():
+    """Class Method to define user interface and error messages which are called from the command line"""
+    def welcome_message(self):
+        """Method to display to the user a welcome message and for them to enter the URL to the CSV"""
+        print('Welcome to the Sky Take Home Test')
+        print('Please enter URL of .csv file')
